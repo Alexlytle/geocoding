16813e9f4e929c5d59009dea8446529fe1fef24e
alex <alex.joe.lytle@gmail.com>
2022-05-23 10:48:37 -0700

    Custom message

---
+++ app.js
@@ -0,0 +1,20 @@
+    
+  
+    const geocode = () => {
+    
+        let location = '2573 claybank rd farifield ca'
+        axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+            params:{
+                address:location,
+                key:'AIzaSyBh9HwdpxpL08FWiEtujQqIY97ZaOq6iKk'
+            }
+        }).then((res=>{
+            console.log(res.data)
+            //formated address
+            console.log(res.data.results[0].formatted_address)
+        }))
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+    geocode()
